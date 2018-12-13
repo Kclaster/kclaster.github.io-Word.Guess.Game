36bf465f70ea0074c09ad4c88d2184ab1e53f645
Kyle Laster <kyle.laster@gmail.com>
2018-12-13 14:59:51 -0600

    added easter egg

---
+++ assets/javascript/game-easter.egg.js
@@ -0,0 +1,13 @@
+var name, userchoice;
+
+document.onkeyup = function (event) {
+    if (typeof userchoice === 'undefined') {
+      userchoice = event.key; 
+    } else {
+        userchoice += event.key;
+    };
+   
+
+    document.getElementById("named-champion").innerHTML = userchoice;
+
+};
